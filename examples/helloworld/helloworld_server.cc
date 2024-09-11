fbe9f316a9fe9a286e45f6fdf1477480c4a5a9f4
2549141519 <2549141519@qq.com>
2024-09-11 18:48:00 +0800

    修改overloadcontroller整体架构

--- examples/helloworld/helloworld_server.cc
+++ examples/helloworld/helloworld_server.cc
@@ -22,7 +22,7 @@
 
 #include "trpc/common/trpc_plugin.h"
 
-#include "trpc/overload_control/smooth_filter/server_flow_controller_server_filter.h"
+#include "trpc/overload_control/smooth_filter/overload_controller_filter.h"
 namespace test {
 
 namespace helloworld {
@@ -45,7 +45,7 @@ class HelloWorldServer : public ::trpc::TrpcApp {
 
   int RegisterPlugins() {
   // register server-side filter
-  auto server_filter = std::make_shared<trpc::overload_control::Server_FlowControlServerFilter>();
+  auto server_filter = std::make_shared<trpc::overload_control::OverloadControlFilter>();
   trpc::TrpcPlugin::GetInstance()->RegisterServerFilter(server_filter);
   return 0;
   }
